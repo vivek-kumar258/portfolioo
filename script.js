a15634fb1d8029ef2b24e1c3e6533e4b87fb83f4
vivek-kumar258 <jhavivek9911@gmail.com>
2025-02-01 19:33:30 +0530

    done

---
+++ script.js
@@ -0,0 +1,4 @@
+var typed = new Typed('#element', {
+    strings: ['<i>  I am a student of B.C.A.</i> ', 'I am a Web Developer  ', 'I have knowledge many programming languages like C, C++, HTML, CSS, Python, Java, Mysql', ],
+    typeSpeed: 70,
+});
